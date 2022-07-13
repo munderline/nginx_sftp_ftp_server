d9745e8941b2125e464952e165d4fe7a6f189d3f
glenda <vip170706@gmail.com>
2022-07-13 09:44:18 +0000

    add default.txt

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ U=${1:-sysadmin}
 P=${2:-sysadmin}
 sftpath="/data/sftp/${U}"
 [ -e ${sftpath} ] || mkdir -p ${sftpath}
+file_path="/data/www/download/"
+[ -d ${file_path} ] || mkdir -p ${file_path}
+dd if=/dev/zero of=${file_path}/default.txt bs=1M count=1
+tmp_file=${file_path}/default.txt
 
 function sftp_in(){
     echo "sftp install..."
@@ -27,6 +31,7 @@ function sftp_in(){
     [ -e ${sftpath_upload} ] || mkdir -p ${sftpath_upload}
     chown ${U}:sftp ${sftpath_upload}
     chmod 755 ${sftpath_upload}
+    ln -sf ${tmp_file} ${sftpath_upload}
     # start...
     systemctl restart sshd && echo -e "\033[5;32m===============sftp start=================\033[0m"
 }
@@ -83,6 +88,7 @@ function ftp_in(){
     echo -e ${U}"\n"${P} > /etc/vsftpd/virtusers
     db_load -T -t hash -f /etc/vsftpd/virtusers /etc/vsftpd/virtusers.db
     mkdir -p /var/ftp/virtual/${U} && chmod 777 -R /var/ftp/virtual/${U}
+    ln -sf ${tmp_file} /var/ftp/virtual/${U}
 
     echo "local_root=/var/ftp/virtual/${U}
     allow_writeable_chroot=YES
